7f8d66696ea1b7b1855dc07cb48d58d312368b1d
Acer <shcherbinsyd@gmail.com>
2020-02-09 17:46:10 +0300

    Created mongoose model photo

--- models/photo.js
+++ models/photo.js
@@ -1,1 +1,16 @@
 const mongoose = require('mongoose');
+
+const photoSchema = new mongoose.Schema({
+  img: {
+    type: String,
+    required: true,
+  },
+  alt: {
+    type: String,
+    required: true,
+  },
+});
+
+const Photo = mongoose.model('Photo', photoSchema);
+
+module.exports.Photo = Photo;
--- routes/photo.js
+++ routes/photo.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const { Photo } = require('../models/photo');
 
 const router = new express.Router();
 
-router.get('/photo', (req, res) => {
-  res.send('photo');
+router.get('/photo', async (req, res) => {
+  try {
+    const photo = await Photo.find();
+    res.send(photo);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+router.post('/photo', async (req, res) => {
+  const photo = new Photo({
+    img: req.body.img,
+    alt: req.body.alt,
+  });
+  await photo.save();
+  res.send(photo);
 });
 
 
