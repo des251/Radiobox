05f873377128c4dea9d122a040e40e01d2826ee3
Acer <shcherbinsyd@gmail.com>
2020-02-09 11:45:40 +0300

    Created get reuests for gallery

---
+++ data.js
@@ -0,0 +1,225 @@
+const data = {
+  images: [
+    {
+      img: 'images/f-image/1.jpg',
+      alt: 'Фото магазина',
+    },
+    {
+      img: 'images/f-image/2.jpg',
+      alt: 'Фото магазина',
+    },
+    {
+      img: 'images/f-image/3.jpg',
+      alt: 'Фото магазина',
+    },
+    {
+      img: 'images/f-image/4.jpg',
+      alt: 'Фото магазина',
+    },
+    {
+      img: 'images/f-image/5.jpg',
+      alt: 'Фото магазина',
+    },
+    {
+      img: 'images/f-image/6.jpg',
+      alt: 'Фото магазина',
+    },
+    {
+      img: 'images/f-image/7.jpg',
+      alt: 'Фото магазина',
+    },
+    {
+      img: 'images/f-image/8.jpg',
+      alt: 'Фото магазина',
+    },
+    {
+      img: 'images/f-image/9.jpg',
+      alt: 'Фото магазина',
+    },
+    {
+      img: 'images/f-image/10.jpg',
+      alt: 'Фото магазина',
+    },
+    {
+      img: 'images/f-image/11.jpg',
+      alt: 'Фото магазина',
+    },
+    {
+      img: 'images/f-image/12.jpg',
+      alt: 'Фото магазина',
+    },
+    {
+      img: 'images/f-image/13.jpg',
+      alt: 'Фото магазина',
+    },
+    {
+      img: 'images/f-image/14.jpg',
+      alt: 'Фото магазина',
+    },
+    {
+      img: 'images/f-image/15.jpg',
+      alt: 'Фото магазина',
+    },
+    {
+      img: 'images/f-image/16.jpg',
+      alt: 'Фото магазина',
+    },
+    {
+      img: 'images/f-image/17.jpg',
+      alt: 'Фото магазина',
+    },
+    {
+      img: 'images/f-image/18.jpg',
+      alt: 'Фото магазина',
+    },
+    {
+      img: 'images/f-image/19.jpg',
+      alt: 'Фото магазина',
+    },
+    {
+      img: 'images/f-image/20.jpg',
+      alt: 'Фото магазина',
+    },
+    {
+      img: 'images/f-image/21.jpg',
+      alt: 'Фото магазина',
+    },
+    {
+      img: 'images/f-image/22.jpg',
+      alt: 'Фото магазина',
+    },
+  ],
+  links: [
+    {
+      link: 'http://2020.rexant.info/#p=1',
+      img: 'images/f-links/2020.jpg',
+      title: 'Каталог REXANT 2020 года',
+    },
+    {
+      link: 'http://2019.rexant.info/#p=1',
+      img: 'images/f-links/2019.jpg',
+      title: 'Каталог REXANT 2019 года',
+    },
+    {
+      link: 'http://video.rexant.info/#p=1',
+      img: 'images/f-links/video.jpg',
+      title: 'Каталог систем видеонаблюдения 2018 года',
+    },
+    {
+      link: 'http://tp.rexant.info/#p=1',
+      img: 'images/f-links/tp.jpg',
+      title: 'Каталог тёплый пол REXANT 2018 года',
+    },
+    {
+      link: 'http://2018.rexant.info/#p=1',
+      img: 'images/f-links/2018.jpg',
+      title: 'Каталог REXANT 2018 года',
+    },
+    {
+      link: 'http://knopki.rexant.info/#p=1',
+      img: 'images/f-links/knopki.jpg',
+      title: 'Кнопки REXANT каталог 2018 года',
+    },
+    {
+      link: 'http://2017.rexant.info/#p=1',
+      img: 'images/f-links/2017.jpg',
+      title: 'Электротехническое оборудование 2017 года',
+    },
+    {
+      link: 'http://pol.rexant.info/#p=1',
+      img: 'images/f-links/pol.jpg',
+      title: 'Тёплый пол 2017',
+    },
+  ],
+  video: [
+    {
+      link: 'https://youtu.be/cB-MsOjrPKE',
+      img: 'https://i.ytimg.com/vi/cB-MsOjrPKE/hqdefault.jpg',
+      title: `Subwoofer 12\" (сабвуфер на 12\" своими
+      руками)`,
+    },
+    {
+      link: 'https://youtu.be/3mIA25pxHdQ',
+      img: 'https://i.ytimg.com/vi/3mIA25pxHdQ/hqdefault.jpg',
+      title: 'Мини Прожектор своими руками',
+    },
+    {
+      link: 'https://youtu.be/TKZQqRai7zE',
+      img: 'https://i.ytimg.com/vi/TKZQqRai7zE/hqdefault.jpg',
+      title: 'Лайтбокс СВОИМИ РУКАМИ',
+    },
+    {
+      link: 'https://youtu.be/EbjAfcVZKNY',
+      img: 'https://i.ytimg.com/vi/EbjAfcVZKNY/hqdefault.jpg',
+      title: 'софт бокс своими руками',
+    },
+    {
+      link: 'https://youtu.be/co2UnjAN5S8',
+      img: 'https://i.ytimg.com/vi/co2UnjAN5S8/hqdefault.jpg',
+      title: 'Bluetooth-Aux-Audio-Receiver Посылка из китая.',
+    },
+    {
+      link: 'https://youtu.be/GyTuRpH_CNc',
+      img: 'https://i.ytimg.com/vi/GyTuRpH_CNc/hqdefault.jpg',
+      title: 'Зарядное для 18650 своими руками',
+    },
+    {
+      link: 'https://youtu.be/FmTRuJmqcgM',
+      img: 'https://i.ytimg.com/vi/FmTRuJmqcgM/hqdefault.jpg',
+      title: 'TWQP Итог акустика',
+    },
+    {
+      link: 'https://youtu.be/DjGD7wJV0Ng',
+      img: 'https://i.ytimg.com/vi/DjGD7wJV0Ng/hqdefault.jpg',
+      title: 'Акустика TQWP своими руками.',
+    },
+    {
+      link: 'https://youtu.be/1esCQcSHtsM',
+      img: 'https://i.ytimg.com/vi/1esCQcSHtsM/hqdefault.jpg',
+      title: 'Как перетянуть стул своими руками',
+    },
+    {
+      link: 'https://youtu.be/G7RVPlnxq1w',
+      img: 'https://i.ytimg.com/vi/G7RVPlnxq1w/hqdefault.jpg',
+      title: 'Чехол для ручника своими руками',
+    },
+    {
+      link: 'https://youtu.be/5c6Wkxmqwxo',
+      img: 'https://i.ytimg.com/vi/5c6Wkxmqwxo/hqdefault.jpg',
+      title: 'Турбо-буржуйка Своими руками',
+    },
+    {
+      link: 'https://youtu.be/iX-bezsUVEo',
+      img: 'https://i.ytimg.com/vi/iX-bezsUVEo/hqdefault.jpg',
+      title: 'ТУРБО-буржуйка испытание',
+    },
+    {
+      link: 'https://youtu.be/dfTFg4ClHjU',
+      img: 'https://i.ytimg.com/vi/dfTFg4ClHjU/hqdefault.jpg',
+      title: 'КАК ПОКРАСИТЬ ТЕЛЕФОН СВОИМИ РУКАМИ',
+    },
+    {
+      link: 'https://youtu.be/t5usdc_n0sI',
+      img: 'https://i.ytimg.com/vi/t5usdc_n0sI/hqdefault.jpg',
+      title: 'Котёл из тэна своими руками. Вся правда!!!',
+    },
+    {
+      link: 'https://youtu.be/GexGJoFgojw',
+      img: 'https://i.ytimg.com/vi/GexGJoFgojw/hqdefault.jpg',
+      title: 'Электростеплер Wortex ES1053 Обзор',
+    },
+    {
+      link: 'https://youtu.be/JohBiwVIgFI',
+      img: 'https://i.ytimg.com/vi/JohBiwVIgFI/hqdefault.jpg',
+      title: 'Замена звукоснимателей и потенциометров на бас гитаре',
+    },
+    {
+      link: 'https://youtu.be/9Yu2XJV5NwQ',
+      img: 'https://i.ytimg.com/vi/9Yu2XJV5NwQ/hqdefault.jpg',
+      title: 'Opel Omega замена ламп приборной панели',
+    },
+  ],
+};
+
+
+module.exports = data;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const data = require('./data');
 
 const app = express();
 
@@ -7,6 +8,16 @@ app.get('/', (req, res) => {
   res.send('Hello');
 });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+app.get('/gallery', (req, res) => {
+  res.send(data);
+});
+
+app.get('/gallery/:category', (req, res) => {
+  const { category } = req.params;
+  res.send(data[category]);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
