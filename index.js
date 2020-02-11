dd5849d586a06d9e64472d008b154c2a5e73cf85
Denis <shcherbinskyd@gmail.com>
2020-02-11 15:33:05 +0300

    Moved cursor to a separate partial hbs

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const photoRouter = require('./routes/photo');
 hbs.registerPartial('head', fs.readFileSync(`${__dirname}/views/partials/head.hbs`, 'utf8'));
 hbs.registerPartial('header', fs.readFileSync(`${__dirname}/views/partials/header.hbs`, 'utf8'));
 hbs.registerPartial('preloader', fs.readFileSync(`${__dirname}/views/partials/preloader.hbs`, 'utf8'));
+hbs.registerPartial('cursor', fs.readFileSync(`${__dirname}/views/partials/cursor.hbs`, 'utf8'));
 
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
