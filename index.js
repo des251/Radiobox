e6629bea5787756b42d8f4b916331d9253ddd883
Denis <shcherbinskyd@gmail.com>
2020-03-24 17:40:52 +0300

    Added rendering 404 error page to the app

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const createError = require('http-errors');
 const path = require('path');
 const hbs = require('hbs');
 const fs = require('fs');
@@ -33,6 +34,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', pagesRouter);
 
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = err;
+
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
