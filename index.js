718fc79bdf134702634acd26a933e10e3907987c
Denis <shcherbinskyd@gmail.com>
2020-03-26 10:34:51 +0300

    Adeded mail route and urlencoded parser

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const hbs = require('hbs');
 const fs = require('fs');
 const pagesRouter = require('./routes/pages');
+const mailRouter = require('./routes/mail');
 
 const partials = [
   'head',
@@ -30,9 +31,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', pagesRouter);
+app.use('/mail', mailRouter);
 
 app.use((req, res, next) => {
   next(createError(404));
