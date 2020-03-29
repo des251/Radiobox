b6f44fbcfd83a332724f5e540a049904dea263b5
Denis <shcherbinskyd@gmail.com>
2020-03-29 11:53:00 +0300

    Created prod middleware for production

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 require('./startup/logging')();
 require('./startup/views')(app);
 require('./startup/routes')(app);
+require('./startup/prod')(app);
 
 
 const port = process.env.PORT || 3000;
